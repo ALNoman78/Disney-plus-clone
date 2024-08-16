be4ea8c0f0447ee48f9176dfb01c6fb18da06e8e
ALNoman78 <abdullaalno078@gmail.com>
2024-08-16 09:04:22 +0600

    using some javascript logic

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ const button = document.getElementById("svg-img");
 button.addEventListener("click", function() {
     this.classList.toggle("hover-effect");
 });
+
+
+let trendContent = document.querySelector(".width-2");
+let math = 0;
+trendContent.addEventListener("click",function(){
+    if (math === 0) {
+        trendContent.style.borderBottom = "3px solid red";
+        console.log("adsa")
+        math = 1;
+
+    }else{
+        trendContent.style.border = "none";
+        math = 0;
+    }
+})
+
+// let contentTwo = document.getElementById("#content-2")
+// contentTwo.addEventListener("click",function(){
+//     contentTwo.style.display = "block"
+//     console.log("Abdullah Al Noman");
+// })
